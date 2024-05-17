a0d5a558dbd446ca8aa20cc1e4cd7bada4cf45d5
Roman Brovkin <rs.brovkin@yandex.ru>
2024-05-17 23:23:37 +0300

    Init

---
+++ logistic/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from logistic.views import ProductViewSet, StockViewSet
+
+router = DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('stocks', StockViewSet)
+
+urlpatterns = router.urls
